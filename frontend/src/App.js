6620a43c5f2b7988c97b86d7b8212026ff099138
77f315 <77f315@gmail.com>
2024-11-20 23:21:10 +0200

    handle image change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import react, {useState} from 'react'
 
 function App() {
+  const [image, setImage] = useState(null);
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+    console.log(e.target.files[0])
+  }
   return (
     <div>
       <h1>
-        Input the image
+        Input board image
       </h1>
-      <form>
-        <input type="file" accept="image/*"/>
+      <form >
+        <input type="file" accept="image/*" onChange={handleImageChange}/>
         <button type="submit">Upload</button>
       </form>
       
